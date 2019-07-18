e51d26c4624de037b1da655a53b3bfce2271e173
EnesRecep <enesrecepcinar@gmail.com>
2019-07-18 23:16:34 +0300

    Cards objects creations added.

--- src/main/java/MonsterCardCreator.java
+++ src/main/java/MonsterCardCreator.java
@@ -2,10 +2,14 @@ import CardModel.Card;
 import CardModel.Civilization;
 import CardModel.Race;
 import CardModel.Type;
-import org.json.simple.JSONArray;
-import org.json.simple.JSONObject;
+import org.apache.commons.io.FileUtils;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
+
+import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
@@ -17,47 +21,58 @@ import java.util.Iterator;
 
 public class MonsterCardCreator {
 
-    public static ArrayList<Card> create(){
+    public static ArrayList<Card> create() {
         ArrayList<Card> cards = new ArrayList<>();
-        Object obj = null;
+
+        File file = new File("C:\\Users\\Enes Recep\\Desktop\\TradingCardGame\\cardsWithPictures.json");
+        String content = null;
         try {
-            obj = new JSONParser().parse(new FileReader("C:\\Users\\Enes Recep\\Desktop\\TradingCardGame\\cardsWithPictures.json"));
-        } catch (ParseException | IOException e) {
+            content = FileUtils.readFileToString(file, "utf-8");
+        } catch (IOException e) {
             e.printStackTrace();
         }
 
-        JSONObject jsonObject = (JSONObject) obj;
+        // Convert JSON string to JSONObject
+        JSONObject jsonObject = new JSONObject(content);
+
         JSONArray ja = (JSONArray) jsonObject.get("cards");
 
-        // iterating phoneNumbers
-        Iterator itr = ja.iterator();
-
-
-        while (itr.hasNext()) {
+        for (int i = 0; i < ja.length(); i++) {
             Card card = new Card();
-            card.setName(((JSONObject)itr).get("name").toString())
-            .setLevel((Integer)((JSONObject)itr).get("level"))
-            .setType(Type.valueOf(((JSONObject)itr).get("type").toString()));
 
-            String[] cardTexts = ((JSONObject)itr).get("card_text").toString().split("\n");
-            for(String str : cardTexts){
-                card.addCardText(str.trim());
+
+            card.setName(ja.getJSONObject(i).getString("name"))
+                    .setLevel(Integer.valueOf(ja.getJSONObject(i).getString("level").trim()))
+                    .setType(Type.valueOf(ja.getJSONObject(i).getString("type").trim().replace(" ", "")));
+
+            if(ja.getJSONObject(i).has("card_text")) {
+                String[] cardTexts = (ja.getJSONObject(i).getString("card_text").split("\n"));
+                for (String str : cardTexts) {
+                    card.addCardText(str.trim());
+                }
             }
 
-            String[] flavorTexts = ((JSONObject)itr).get("flavor_text").toString().split("\n");
-            for(String str : flavorTexts){
-                card.addFlavorText(str.trim());
+            if(ja.getJSONObject(i).has("flavor_text")) {
+                String[] flavorTexts = (ja.getJSONObject(i).getString("flavor_text").split("\n"));
+                for (String str : flavorTexts) {
+                    card.addFlavorText(str.trim());
+                }
             }
 
-            String[] civilizations = ((JSONObject)itr).get("civilization").toString().split("/");
-            for(String str : civilizations){
+            String[] civilizations = (ja.getJSONObject(i).getString("civilization").split("/"));
+            for (String str : civilizations) {
                 card.addCivilization(Civilization.valueOf(str.trim()));
             }
 
-            if(card.getType().equals(Type.Spell)){
+            if (card.getType().equals(Type.Creature) || card.getType().equals(Type.EvolutionCreature)) {
                 card.
-                        setPower((Integer)((JSONObject)itr).get("power"))
-                        .setRace(Race.valueOf(((JSONObject)itr).get("race").toString()));
+                        setPower(Integer.valueOf(ja.getJSONObject(i).getString("power").trim()));
+
+                String[] races = (ja.getJSONObject(i).getString("race").split("/"));
+                for (String str : races) {
+                    card.addRace(str.trim());
+                }
+
             }
 
             cards.add(card.build());
